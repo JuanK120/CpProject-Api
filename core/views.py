e73aadcc6791c0c6d3a340d9bbf460399d723cc0
JuanK120 <juan.camilo.rosero@correounivalle.edu.co>
2022-06-23 19:20:11 -0500

    test of django

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
+import json
 from django.shortcuts import render
+from django.http import HttpResponse
+from json import *
 
 # Create your views here.
+
+def test(request):
+    ret = """<html><body><strong>this is a test!!</strong></body></html>""" 
+    return HttpResponse(ret)
