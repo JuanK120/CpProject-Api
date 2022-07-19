376bc3088da5ec72b04a89efb0f982e08cb87ef1
JuanK120 <juan.camilo.rosero@correounivalle.edu.co>
2022-07-18 20:13:17 -0500

    views creation

--- core/minizinc/handler.py
+++ core/minizinc/handler.py
@@ -4,6 +4,9 @@ from importlib.resources import path
 from statistics import mode
 from unittest import result
 import pymzn
+import asyncio
+from pymzn.aio import minizinc
+import config 
 #testModel = Model("./models/test.mzn")
 
 #cronogramaEnsayo = Model('./models/cronogramaEnsayos.mzn')
@@ -12,15 +15,20 @@ import pymzn
 
 
 
-def testCase(num):
-    solucion = pymzn.minizinc('./core/minizinc/models/testCase.mzn', data={"i":num})
-    ret = solucion[0]
+def RunTestCase(num):
+
+    solucion = pymzn.minizinc('./core/minizinc/models/testCase.mzn', 
+                                data={
+                                    'i' : num
+                                    })
+    ret = [solucion[0],solucion.status]
     return ret
 
 def cronogramaEnsayoWithForm(args):
-    solucion = pymzn.minizinc('./core/minizinc/models/testCase.mzn', 
-                                data={'Actores' : args[0],
+    
+    solucion = pymzn.minizinc('./core/minizinc/models/cronogramaEnsayos.mzn', data={
                                       'NumActores' : args[1],
+                                      'Actores' : args[0],
                                       'NumEscenas' : args[2],
                                       'Escenas' : args[3],
                                       'Duracion' : args[4],
@@ -28,13 +36,21 @@ def cronogramaEnsayoWithForm(args):
                                       'Disponibilidad' : args[6],
                                       'NumEvitaciones' : args[7],
                                       'Evitar': args[8],
-                                    })
-    ret = solucion[0]
+                                    }
+    )
+    if solucion.status is pymzn.Status.COMPLETE :
+        ret = [solucion[0],"Complete"]
+    if solucion.status is pymzn.Status.UNSATISFIABLE :
+        ret = [[],"Unsatisfiable"]
+
     return ret
 
-def cronogramaEnsayoWithForm(file):
+def cronogramaEnsayoWithFile(file):
     solucion = pymzn.minizinc('./core/minizinc/models/testCase.mzn',file)
-    ret = solucion[0]
+    if solucion.status is pymzn.Status.COMPLETE :
+        ret = [solucion[0],"Complete"]
+    if solucion.status is pymzn.Status.UNSATISFIABLE :
+        ret = [[],"Unsatisfiable"]
     return ret
 
     
--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from core.minizinc.handler import testCase, cronogramaEnsayoWithForm
+from core.minizinc.handler import RunTestCase, cronogramaEnsayoWithForm, cronogramaEnsayoWithFile
+import asyncio
+import config
 
 # Create your views here.
 
@@ -9,16 +11,23 @@ from core.minizinc.handler import testCase, cronogramaEnsayoWithForm
 def test(request):
     req=json.load(request)
     i = req["i"]
-    ret = testCase(i)
+    ret = RunTestCase(i)
+    config.Solution=ret
     return JsonResponse(ret,safe=False)
 
 @csrf_exempt
-def getResultWithForm(request):
+def getTest(request):
+    
+    return JsonResponse(config.Solution,safe=False)
+
+@csrf_exempt
+def runWithForm(request):
     req=json.load(request)
     args = []
     args.append(req['Actores'])
     args.append(req['NumActores'])
     args.append(req['NumEscenas'])
+    args.append(req['Escenas'])
     args.append(req['Duracion'])
     args.append(req['NumActoresConDisponibilidad'])
     args.append(req['Disponibilidad'])
@@ -28,18 +37,11 @@ def getResultWithForm(request):
     return JsonResponse(ret,safe=False)
 
 @csrf_exempt
-def uploadFileAndSolve(request):
-    res = JsonResponse({})
-    return res
+def runWithFile(request):
+    req=json.load(request)
+    ret = cronogramaEnsayoWithFile(req['file'])
+    return JsonResponse(ret,safe=False)
 
 @csrf_exempt
-def getStoredFiles(request):
-    res = JsonResponse({})
-    return res
-
-@csrf_exempt
-def getResultWithUploadedFile(request):
-    res = JsonResponse({})
-    return res
-
-
+def getSolution(request):
+    return JsonResponse(config.Solution,safe=False)
